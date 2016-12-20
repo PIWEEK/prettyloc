671d9e8de8850b5b25db0ccb28af78a2571f0a0d
marpesia <marpesian@gmail.com>
2016-12-20 13:12:16 +0100

    clean js and simple function on map

--- front/js/app.js
+++ front/js/app.js
@@ -3,39 +3,14 @@ $(document).ready(function() {
 	var map = L.map('map').setView([39.74739, -105], 15);
 	title(map);
 
-    //icon('./assets/baseball-marker.png', [32, 37], [16, 37], [0, -28], map);
+    var line1;
 
-    
-    var marker = marker([39.74739, -105], map);
-
-    marker.on('mouseover', function() {
-        console.log('hola');
-        line(routes, map);
-    });
-
-/*	L.geoJSON([bicycleRental, campus], {
-
-		style: function (feature) {
-			return feature.properties && feature.properties.style;
-		},
-
-		onEachFeature: onEachFeature,
-
-		pointToLayer: function (feature, latlng) {
-			return L.circleMarker(latlng, {
-				radius: 8,
-				fillColor: "#ff7800",
-				color: "#000",
-				weight: 1,
-				opacity: 1,
-				fillOpacity: 0.8
-			});
-		}
-	}).addTo(map);*/
-
-    
-
-
+    L.marker([39.75383843460583, -105.00341892242432])
+        .on('click', function() {
+            line1 = line(routes, map);
+        })
+        .bindPopup('<a onclick="hideLine('+ this +','+ map +')">ocultar ruta</a>').openPopup()
+        .addTo(map);
 
 });
 
@@ -49,55 +24,10 @@ function title(map) {
 	}).addTo(map);
 }
 
-function icon(url, size, anchor, popup, map) {
-    var iconData = L.icon({
-		iconUrl: url,
-		iconSize: size,
-		iconAnchor: anchor,
-		popupAnchor: popup
-	});
-
-	var coorsLayer = L.geoJSON(coorsField, {
-
-		pointToLayer: function (feature, latlng) {
-			return L.marker(latlng, {icon: iconData});
-		},
-
-		onEachFeature: onEachFeature
-	}).addTo(map);
-}
-
 function line(data, map) {
-	L.geoJSON(data, {
-
-/*		filter: function (feature, layer) {
-			if (feature.properties) {
-				// If the property "underConstruction" exists and is true, return false (don't render features under construction)
-				return feature.properties.underConstruction !== undefined ? !feature.properties.underConstruction : true;
-			}
-			return false;
-		},
-
-		onEachFeature: onEachFeature*/
-	}).addTo(map);
+	return L.geoJSON(data).addTo(map);
 }
 
-function area(map) {
-
-}
-
-function circle(map) {
-
-}
-
-function marker(lnglat, map) {
-    console.log(lnglat, 'log');
-    L.marker(lnglat).addTo(map);
-}
-
-function onEachFeature(feature, layer) {
-    var popupContent = "<p>I started out as a GeoJSON " +
-            feature.geometry.type + ", but now I'm a Leaflet vector!</p>";
-
-    layer.bindPopup(popupContent);
+function hideLine(line, map) {
+    map.removeLayer(line);
 }
