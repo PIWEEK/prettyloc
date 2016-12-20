01de86e9148ef6d6a6b8c8cf3f4bda708d12537c
Yamila Moreno <yamila.ms@gmail.com>
2016-12-20 15:44:17 +0100

    IT WORKS!!!! \o/ @marpesia

--- front/js/app.js
+++ front/js/app.js
@@ -1,26 +1,32 @@
 $(document).ready(function() {
 
-	var map = L.map('map').setView([39.74739, -105], 15);
+    var denver = [39.75383843460583, -105.00341892242432];
+	var map = L.map('map').setView(denver, 15);
 	title(map);
+    var sidebar = L.control.sidebar('sidebar').addTo(map);
 
     var line1;
 
-    L.marker([39.75383843460583, -105.00341892242432])
-        .on('click', function() {
+    L.marker(denver)
+        .on('mouseover', function() {
             line1 = line(routes, map);
+        })
+        .on('mouseout', function(){
+            map.removeLayer(line1);
         })
         .bindPopup('<a onclick="hideLine('+ this +','+ map +')">ocultar ruta</a>').openPopup()
         .addTo(map);
 
 });
 
+// choose map providers between: https://leaflet-extras.github.io/leaflet-providers/preview/
 function title(map) {
 	return L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		id: 'mapbox.light'
+		id: 'mapbox.outdoors'
 	}).addTo(map);
 }
 
--- front/js/data.js
+++ front/js/data.js
@@ -6,30 +6,16 @@ var routes = {
           "geometry": {
               "type": "LineString",
               "coordinates": [
-                  [-105.00341892242432, 39.75383843460583],
-                  [-105.0008225440979, 39.751891803969535]
-              ]
-          },
-          "properties": {
-              "popupContent": "This is free bus that will take you across downtown.",
-              "underConstruction": false
-          },
-          "id": 1
-      },
-      {
-          "type": "Feature",
-          "geometry": {
-              "type": "LineString",
-              "coordinates": [
+                  [-104.99820470809937, 39.74979664004068],
                   [-105.0008225440979, 39.751891803969535],
-                  [-104.99820470809937, 39.74979664004068]
+                  [-105.00341892242432, 39.75383843460583],
               ]
           },
           "properties": {
               "popupContent": "This is free bus that will take you across downtown.",
               "underConstruction": true
           },
-          "id": 2
+          "id": 1
       },
   ]
 };
