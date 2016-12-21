28ca21b9c32a598de1d664269628133f38a7b2bb
Yamila Moreno <yamila.ms@gmail.com>
2016-12-21 10:04:11 +0100

    Add django cors headers

--- back/project/settings.py
+++ back/project/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_gis',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,6 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 #REST_FRAMEWORK = {
 #    'DEFAULT_PERMISSION_CLASSES': [
