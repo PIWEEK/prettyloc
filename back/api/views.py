8385821cffc07daa179c3a94f0de3ba9bbc2c200
Yamila Moreno <yamila.ms@gmail.com>
2016-12-22 11:32:22 +0100

    Add 'others' filter to route_type

--- back/api/views.py
+++ back/api/views.py
@@ -6,6 +6,18 @@ from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework_gis.filters import InBBoxFilter, DistanceToPointFilter
 
+
+types_with_icon = [
+        'hiking',
+        'walking',
+        'nordic-walking',
+        'mountain-biking',
+        'cyclocross',
+        'cycling',
+        'bicycle-touring',
+        'running trail-running'
+]
+
 class RouteView(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows view routes
@@ -17,3 +29,10 @@ class RouteView(viewsets.ReadOnlyModelViewSet):
     distance_filter_field = 'start_point'
     bbox_filter_field = 'start_point'
     bbox_filter_include_overlapping = True
+
+    def get_queryset(self):
+        queryset = Route.objects.all()
+        route_types = self.request.query_params.getlist('route_type', None)
+        if 'others' in route_types:
+            queryset = queryset.exclude(route_type__in=types_with_icon)
+        return queryset
