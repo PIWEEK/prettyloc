01676f745482a4f4d1a5a48e433521b8e3ca2940
Yamila Moreno <yamila.ms@gmail.com>
2016-12-22 13:02:35 +0100

    Fix route_type filter

--- back/api/filters.py
+++ back/api/filters.py
@@ -1,6 +1,17 @@
 import django_filters
 from api.models import Route, DIFFICULTIES, TYPES
 
+types_with_icon = [
+        'hiking',
+        'walking',
+        'nordic-walking',
+        'mountain-biking',
+        'cyclocross',
+        'cycling',
+        'bicycle-touring',
+        'running trail-running'
+]
+
 class RouteFilter(django_filters.FilterSet):
     min_dist = django_filters.NumberFilter(name="route_length", lookup_expr='gte')
     max_dist = django_filters.NumberFilter(name="route_length", lookup_expr='lte')
@@ -9,10 +20,19 @@ class RouteFilter(django_filters.FilterSet):
             lookup_expr='in',
             choices=DIFFICULTIES)
     route_type = django_filters.MultipleChoiceFilter(
+            method='route_type_filter',
             name="route_type",
-            lookup_expr='in',
-            choices=TYPES)
+            choices=TYPES + (('other', 'other'),))
 
     class Meta:
         model = Route
         fields = ['route_loop', 'route_type', 'min_dist', 'max_dist', 'technical_difficulty']
+
+    def route_type_filter(self, queryset, name, choices):
+        route_type = self.request.query_params.getlist('route_type', None)
+        if route_type:
+            if 'other' in route_type:
+                queryset = queryset.exclude(route_type__in=types_with_icon)
+            else:
+                queryset = queryset.filter(route_type__in=route_type)
+        return queryset
--- back/api/views.py
+++ back/api/views.py
@@ -7,17 +7,6 @@ from rest_framework import viewsets
 from rest_framework_gis.filters import InBBoxFilter, DistanceToPointFilter
 
 
-types_with_icon = [
-        'hiking',
-        'walking',
-        'nordic-walking',
-        'mountain-biking',
-        'cyclocross',
-        'cycling',
-        'bicycle-touring',
-        'running trail-running'
-]
-
 class RouteView(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows view routes
@@ -29,10 +18,3 @@ class RouteView(viewsets.ReadOnlyModelViewSet):
     distance_filter_field = 'start_point'
     bbox_filter_field = 'start_point'
     bbox_filter_include_overlapping = True
-
-    def get_queryset(self):
-        queryset = Route.objects.all()
-        route_types = self.request.query_params.getlist('route_type', None)
-        if 'other' in route_types:
-            queryset = queryset.exclude(route_type__in=types_with_icon)
-        return queryset
