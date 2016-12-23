1cc0d9d1a33215fe84a1fe2c928faa0c2b57759e
Yamila Moreno <yamila.ms@gmail.com>
2016-12-23 11:12:15 +0100

    Less rest for REST, more rest for @pablo.alba (joke!)

--- back/project/settings.py
+++ back/project/settings.py
@@ -128,7 +128,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'DEFAULT_PAGINATION_CLASS': 'api.pagination.LinkHeaderPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    #'DEFAULT_PAGINATION_CLASS': 'api.pagination.LinkHeaderPagination',
     'PAGE_SIZE': 1000,
 }
